fdc59af55a08b5fe12165e91193e0092e0e05503
Talkwarrior <baekjonghoen@gmail.com>
2020-06-06 16:25:00 +0900

    merged main.ui into  mainWindow.py This will improve update speed.

--- GUI/mainWindow.py
+++ GUI/mainWindow.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtGui, QtWidgets
+from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5 import uic
 import os
 from utils import pyTask
@@ -16,31 +16,113 @@ class MainWindow(QtWidgets.QMainWindow):
         self.setEventListener()
 
     def loadUi(self):
-        uic.loadUi("UI/main.ui", self)
+        # uic.loadUi("UI/main.ui", self)
+        self.setupUi()
         self.updateTable()
 
-        # init menubar
-        menubar = self.menuBar
-        menubar.setNativeMenuBar(False)
-        filemenu = menubar.addMenu('&File')
-        viewmenu = menubar.addMenu('&View')
+    def setupUi(self):
+        self.setObjectName("MainWindow")
+        self.resize(670, 547)
+        self.setMinimumSize(QtCore.QSize(670, 400))
+        self.setMaximumSize(QtCore.QSize(670, 800))
+        self.setDocumentMode(False)
+        self.setTabShape(QtWidgets.QTabWidget.Triangular)
+        self.setDockNestingEnabled(False)
+        self.setDockOptions(QtWidgets.QMainWindow.AnimatedDocks)
+        self.setUnifiedTitleAndToolBarOnMac(False)
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.taskTable = QtWidgets.QTableWidget(self.centralwidget)
+        self.taskTable.setGeometry(QtCore.QRect(20, 20, 531, 476))
+        self.taskTable.setMinimumSize(QtCore.QSize(0, 0))
+        self.taskTable.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.taskTable.setDragDropMode(QtWidgets.QAbstractItemView.NoDragDrop)
+        self.taskTable.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self.taskTable.setShowGrid(True)
+        self.taskTable.setWordWrap(False)
+        self.taskTable.setCornerButtonEnabled(True)
+        self.taskTable.setRowCount(23)
+        self.taskTable.setColumnCount(4)
+        self.taskTable.setObjectName("taskTable")
 
-        actionSave = QtWidgets.QAction(QtGui.QIcon("data/images/save.png"), "Save", self)
-        actionSave.setShortcut('Ctrl+S')
-        actionSave.setStatusTip('Save File')
-        actionSave.triggered.connect(self.ASave)
-        filemenu.addAction(actionSave)
+        item = QtWidgets.QTableWidgetItem()
+        item.setTextAlignment(QtCore.Qt.AlignCenter)
+        self.taskTable.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        item.setTextAlignment(QtCore.Qt.AlignCenter)
+        self.taskTable.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        item.setTextAlignment(QtCore.Qt.AlignCenter)
+        self.taskTable.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        item.setTextAlignment(QtCore.Qt.AlignCenter)
+        self.taskTable.setHorizontalHeaderItem(3, item)
+        self.taskTable.horizontalHeader().setCascadingSectionResizes(True)
+        self.taskTable.horizontalHeader().setDefaultSectionSize(120)
+        self.addButton = QtWidgets.QPushButton(self.centralwidget)
+        self.addButton.setGeometry(QtCore.QRect(560, 20, 93, 28))
+        self.addButton.setObjectName("addButton")
+        self.delButton = QtWidgets.QPushButton(self.centralwidget)
+        self.delButton.setGeometry(QtCore.QRect(560, 60, 93, 28))
+        self.delButton.setObjectName("delButton")
 
-        viewCoWork = QtWidgets.QAction("CoWork only", self)
-        viewCoWork.setCheckable(True)
-        viewCoWork.toggled.connect(self.updateTable)
-        viewmenu.addAction(viewCoWork)
+        self.setCentralWidget(self.centralwidget)
+        self.statusBar = QtWidgets.QStatusBar(self)
+        self.statusBar.setObjectName("statusBar")
+        self.setStatusBar(self.statusBar)
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 670, 26))
+        self.menubar.setObjectName("menubar")
+        self.filemenu = QtWidgets.QMenu(self.menubar)
+        self.filemenu.setObjectName("filemenu")
+        self.viewmenu = QtWidgets.QMenu(self.menubar)
+        self.viewmenu.setObjectName("viewmenu")
+        self.setMenuBar(self.menubar)
+        self.actionSave = QtWidgets.QAction(self)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../data/images/save.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionSave.setIcon(icon)
+        self.actionSave.setObjectName("actionSave")
+        self.viewCoWork = QtWidgets.QAction(self)
+        self.viewCoWork.setCheckable(True)
+        self.viewCoWork.setObjectName("viewCoWork")
+        self.filemenu.addAction(self.actionSave)
+        self.viewmenu.addAction(self.viewCoWork)
+        self.menubar.addAction(self.filemenu.menuAction())
+        self.menubar.addAction(self.viewmenu.menuAction())
 
-    def updateTable(self, co_work_Only=False):
-        self.taskTable.clear()
+        self.retranslateUi()
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+
+    def retranslateUi(self):
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("self", "과제독촉기"))
+        self.taskTable.setSortingEnabled(False)
+        item = self.taskTable.horizontalHeaderItem(0)
+        item.setText(_translate("self", "이름"))
+        item = self.taskTable.horizontalHeaderItem(1)
+        item.setText(_translate("self", "제출기한"))
+        item = self.taskTable.horizontalHeaderItem(2)
+        item.setText(_translate("self", "필요시간"))
+        item = self.taskTable.horizontalHeaderItem(3)
+        item.setText(_translate("self", "분류"))
+        self.addButton.setText(_translate("self", "추가"))
+        self.delButton.setText(_translate("self", "삭제"))
+        self.filemenu.setTitle(_translate("self", "File"))
+        self.viewmenu.setTitle(_translate("self", "View"))
+        self.actionSave.setText(_translate("self", "Save"))
+        self.actionSave.setShortcut(_translate("self", "Ctrl+S"))
+        self.viewCoWork.setText(_translate("self", "CoWork Only"))
+
+    # TODO: default value is wrong
+    def updateTable(self):
+        self.taskTable.clearContents()
         elem = ['label', 'deadline', 'require', 'type']
 
         row = 0
+        # FIXME: need more simple code
+        co_work_Only = self.viewCoWork.isChecked()
         if co_work_Only:
             for task in self.tasks:
                 if co_work_Only and task.co_work:
@@ -58,6 +140,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.addButton.clicked.connect(self.AAdd)
         self.delButton.clicked.connect(self.ADelete)
         self.taskTable.doubleClicked.connect(self.tableDblClicked)
+        self.viewCoWork.toggled.connect(self.updateTable)
 
     def loadData(self):
         if os.path.isfile('data/tasks.json'):
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import sys
 from GUI.mainWindow import MainWindow
 from PyQt5 import QtWidgets
 
 if __name__ == '__main__':
+    import sys
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
+
     window.show()
-    app.exec_()
+    sys.exit(app.exec_())
